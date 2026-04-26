2345eee8d17a2b78d2b78679caa638a1603d3c28
ggupta148 <guptagaurav2503@gmail.com>
2026-04-26 18:04:39 +0530

    add get-users route to fetch all users from the database

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { register } from "../controllers/authController.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.get("/get-users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 export default router;
