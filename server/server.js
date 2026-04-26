bb452e95c1d9ab85446861ff4df9149f8b59b08e
ggupta148 <guptagaurav2503@gmail.com>
2026-04-26 17:47:09 +0530

    mongodb connection added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const PORT = 5000;
+dotenv.config();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
